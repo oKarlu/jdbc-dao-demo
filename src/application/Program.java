a0da88a5b8a7180fca6e3022c8afc7d4c12f5362
Carlos Marinho <cadumsantos@hotmail.com>
2022-03-28 04:36:54 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -35,10 +34,18 @@ public class Program {
 			System.out.println();
 		}
 		
-		System.out.println("\n=== TEST 4: seller insert =====");
+		/*System.out.println("\n=== TEST 4: seller insert =====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
-		System.out.println("Inserted! new Id: " + newSeller.getId());
+		System.out.println("Inserted! new Id: " + newSeller.getId());*/
+		
+		System.out.println("\n=== TEST 5: seller update =====");
+		seller = sellerDao.findById(9);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
+		
 
 	}
 
